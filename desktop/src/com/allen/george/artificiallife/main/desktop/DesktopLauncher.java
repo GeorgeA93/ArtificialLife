3debd846080d325c26385202fd61f9f556b83a92
GeorgeA93 <george.allen93@outlook.com>
2014-06-22 19:38:06 +0100

    Project Setup

---
+++ desktop/src/com/allen/george/artificiallife/main/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.allen.george.artificiallife.main.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.allen.george.artificiallife.main.ArtificialLife;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ArtificialLife(), config);
+	}
+}
